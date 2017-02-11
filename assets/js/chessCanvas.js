7df228a52343c8fd579d3ec3925957f04be89c47
Bijoy Anupam <bijoy.anupam@gmail.com>
2017-02-11 18:45:34 +0530

    [CBG-5] - Add logic to draw the chess board with appropriate chess color.

--- assets/js/chessCanvas.js
+++ assets/js/chessCanvas.js
@@ -9,6 +9,8 @@ var ChessBoard = {
         'boardSize': 800,
     },
 
+    'currentCellColor': chessConfig.boardCellWhiteIdentifier,
+
     'cellWidth': 0,
 
     'cellHeight': 0,
@@ -24,6 +26,7 @@ var ChessBoard = {
         this.cellWidth = this.config.boardSize / chessConfig.boardCellNumber;
         this.cellHeight = this.config.boardSize / chessConfig.boardCellNumber;
 
+
         this.drawBoard();
     },
 
@@ -33,18 +36,47 @@ var ChessBoard = {
      * @return void
      */
     'drawBoard': function () {
+        var cellCount,
+            totalCellCount = chessConfig.boardCellNumber * chessConfig.boardCellNumber;
+
         debugConsole('Draw the chess canvas.');
+        this.$boardElm.empty();
         this.$boardElm.height(this.config.boardSize);
         this.$boardElm.width(this.config.boardSize);
+
+        for (cellCount = 1; cellCount <= totalCellCount; cellCount++) {
+            this.drawCell(cellCount);
+        }
     },
 
     /**
      * Draw the chess cell.
      *
+     * @param cellCount string Current cell count.
+     * 
      * @return void
      */
-    'drawCell': function () {
+    'drawCell': function (cellCount) {
         debugConsole('Draw the chess cell.');
+        $('<div class="' + chessConfig.boardCellIdentifier + '"></div>').appendTo(this.$boardElm)
+            .width(this.cellWidth - 2)
+            .height(this.cellHeight - 2)
+            .addClass(this.getCellColor(cellCount));
+    },
+
+    /**
+     * Draw the colored chess cell with approproate color.
+     *
+     * @param cellCount string Current cell count.
+     * 
+     * @return string Current cell className.
+     */
+    'getCellColor': function (cellCount) {
+        if (((cellCount - 1) % chessConfig.boardCellNumber) === 0) {
+            this.currentCellColor = (this.currentCellColor === chessConfig.boardCellWhiteIdentifier) ? chessConfig.boardCellBlackIdentifier : chessConfig.boardCellWhiteIdentifier;
+        }
+        this.currentCellColor = (this.currentCellColor === chessConfig.boardCellWhiteIdentifier) ? chessConfig.boardCellBlackIdentifier : chessConfig.boardCellWhiteIdentifier;
+        return this.currentCellColor;
     },
 };
 
--- assets/js/chessConfig.js
+++ assets/js/chessConfig.js
@@ -6,7 +6,10 @@ var chessConfig = {
     'debug': true,
     'boardType': '8x8',
     'boardCellNumber': 8,
-    'boardIdentifier': '.chessboard'
+    'boardIdentifier': '.chessboard',
+    'boardCellIdentifier': 'cbg-cell',
+    'boardCellWhiteIdentifier': 'cbg-white',
+    'boardCellBlackIdentifier': 'cbg-black'
 };
 
 // My debug console.
